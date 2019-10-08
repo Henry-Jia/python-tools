2cb2342ab917b46c5e9a9bdf2b79eeeb15ed8ec6
Kevin Sunny Parasseril <kevinsunny1996@gmail.com>
2019-10-08 15:50:42 +0530

    Added a python program for power billing.
    Calculates electricity consumed

---
+++ electricity_consumption.py
@@ -0,0 +1,22 @@
+from math import *
+unit=input("Enter the units consumed in watts")
+if unit>50:
+	print "amount is"
+	print 25*unit
+elif unit>100:
+	print "amount is"
+	print 50*unit
+else:
+	print "amount is"
+	print unit
+#Enter the units consumed in watts101
+#amount is
+#2525
+
+#Enter the units consumed in watts49
+#amount is
+#49
+
+#Enter the units consumed in watts75
+#amount is
+#1875
