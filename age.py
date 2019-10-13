75c0cac767e211345f871a7afdb2e083d2820672
Mukesh Sahani <52211262+MkshSahani@users.noreply.github.com>
2019-10-13 10:37:27 +0530

    Update age.py
    added the show_data() method to Formula class.
    add __name__ variable to your script so you can.
    add error handling the the age method.

--- age.py
+++ age.py
@@ -2,13 +2,22 @@ import datetime as dt
 
 class formula:
     def __init__(self, first_name, second_name, birthdate_year):
-        self.second_name=str(second_name).upper()
-        self.first_name=str(first_name).upper()
+        self.second_name=str(second_name).title() # title text format looks good.
+        self.first_name=str(first_name).title()
         self.birthdate=birthdate_year
 
     def age(self):
-        return dt.datetime.today().year-self.birthdate
+        try:
+            result_age dt.datetime.today().year-self.birthdate
+            return result_age
+        execpt:
+            print("Some Error occured")
+            
+    def show_data(self):
+        print(f"Name  : {self.first_name} {self.second_name} Age : {self.age()}") # method for get the data.
 
-
-age=formula("Lim", "Boris", 1987)
-print(age.age())
+        
+if __name__ == '__main__': # add __name__ variable so you can use this script a module without any bug.
+    age=formula("Lim", "Boris", 1987)
+    print(age.age())
+    age.show_data()
